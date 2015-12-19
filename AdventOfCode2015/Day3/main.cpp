17968196bf12baff30b6ee6fc72cc18a3c7a1b50
arcanumghost <arcanumghost@gmail.com>
2015-12-19 17:20:35 -0600

    Day 3

--- AdventOfCode2015/Day3/main.cpp
+++ AdventOfCode2015/Day3/main.cpp
@@ -1,8 +1,47 @@
 #include <iostream>
 #include <fstream>
+#include <map>
+
+using namespace std;
 
 int main()
 {
+	map<pair<int, int>,int> houses;
+	fstream input {"input.txt", ios::in | ios::binary};
+
+	char c;
+	pair<int, int> santa = {0,0};
+	pair<int, int> roboSanta = {0,0};
+	houses[santa] = 2;
+	while (input >> c)
+	{
+		// Update location
+		switch (c)
+		{
+			case '^':
+				santa.first++;
+				break;
+			case 'v':
+				santa.first--;
+				break;
+			case '>':
+				santa.second++;
+				break;
+			case '<':
+				santa.second--;
+				break;
+		}
+
+		houses.find(santa) == houses.end() ? houses[santa] = 0 : houses[santa]++;
+
+		swap(santa, roboSanta);
+	}
+
+	cout << houses.size();
+
+	// Stop
+	char x;
+	cin >> x;
 
 	return 0;
 }
