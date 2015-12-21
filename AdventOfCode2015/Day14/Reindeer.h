59a2dbba44eb6dbf4e65880f3e27c6ef27b56922
arcanumghost <arcanumghost@gmail.com>
2015-12-20 18:25:57 -0600

    Day 14 Partial

---
+++ AdventOfCode2015/Day14/Reindeer.h
@@ -0,0 +1,18 @@
+#pragma once
+
+class Reindeer
+{
+	int mSpeed;		// km/s while flying
+	int mEndurance;	// s flying before resting
+	int mRecharge;	// s resting before flying again
+
+	bool bFlying;	// State
+	int time;		// Time remaining in state
+
+	int mPosition;	// Distance traveled
+
+public:
+	Reindeer(int speed, int endurance, int recharge);
+	void run();
+	int 
+};
