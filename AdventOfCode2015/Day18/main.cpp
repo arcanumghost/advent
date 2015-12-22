3d6cf7768bab556bb624d5cfdb4772f677b2640b
arcanumghost <arcanumghost@gmail.com>
2015-12-22 16:41:29 -0600

    Day18 save

--- AdventOfCode2015/Day18/main.cpp
+++ AdventOfCode2015/Day18/main.cpp
@@ -10,13 +10,15 @@ using namespace std;
 bool newSwitch(int x, int y, bool *past)
 {
 	int adj = 0;
-	for(int i=max(0,x-1);i<min(100,x+1);++i)
-		for(int j=max(0,y-1);j<min(100,y+1);++j)
+	for(int i=max(0,x-1);i<=min(99,x+1);++i)
+		for(int j=max(0,y-1);j<min(99,y+1);++j)
 			if(i!=x && j!=y)
 				adj += past[i*100 + j];
 	if(past[y*100 + x] && (adj == 2 || adj == 3))
 		return true;
-	if(!past[y*100] + x] && adj == )
+	if(!past[y*100 + x] && adj == 3)
+		return true;
+	return false;
 }
 
 int main()
@@ -26,13 +28,26 @@ int main()
 	string line;
 	bool *switches = new bool[100*100];
 	bool *buffer = new bool[100*100];
+	int y=0;
 	while (getline(input, line))
 	{
-		
+		for(int i=0; i<100; ++i)
+		{
+			switches[i + y*100] = (line[i] == '#');
+		}
+		y++;
 	}
 	input.close();
 
 	// Simulate
+	for (int i = 0; i < 100; ++i)
+	{
+		for(int x=0; x<100; ++x)
+			for(int y=0; y<100; ++y)
+				buffer[y*100 + x] = newSwitch(x,y,switches);
+
+		swap(buffer, switches);
+	}
 
 	// Print
 	int nSwitches = 0;
