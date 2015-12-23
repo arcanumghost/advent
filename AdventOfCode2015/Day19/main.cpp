344ed791c5399d02c41e51b51260f34c7fb1da3e
arcanumghost <arcanumghost@gmail.com>
2015-12-23 14:24:28 -0600

    Day19 Heuristic failing
    the attempt, gotta think of other ideas

--- AdventOfCode2015/Day19/main.cpp
+++ AdventOfCode2015/Day19/main.cpp
@@ -4,8 +4,18 @@
 #include <vector>
 #include <fstream>
 #include <map>
+#include <algorithm>
 
 using namespace std;
+
+void reduce(set<string> &sources)
+{
+	vector<string> sortedsources(sources.begin(),sources.end());
+	sort(sortedsources.begin(),sortedsources.end());
+	sources.clear();
+	for(int i=0; i<50000 && i<sortedsources.size(); ++i)
+		sources.insert(sortedsources[i]);
+}
 
 void step(set<string> &sources, vector<pair<string,string>> &replacements)
 {
@@ -25,12 +35,18 @@ void step(set<string> &sources, vector<pair<string,string>> &replacements)
 		}
 	}
 	sources = results;
-	cout << results.size() << " sources" << endl;
+	cout << results.size() << " sources ";
+	reduce(sources);
+	cout << "reduced to " << sources.size() << endl;
+	if(!sources.size())
+		cout << "damn" << endl;
 }
 
+
+// between 207 and 215...
 int main()
 {
-	fstream input {"input.txt",ios::in};
+	fstream input {"test.txt",ios::in};
 	string line;
 
 	//string dest = "CRnCaCaCaSiRnBPTiMgArSiRnSiRnMgArSiRnCaFArTiTiBSiThFYCaFArCaCaSiThCaPBSiThSiThCaCaPTiRnPBSiThRnFArArCaCaSiThCaSiThSiRnMgArCaPTiBPRnFArSiThCaSiRnFArBCaSiRnCaPRnFArPMgYCaFArCaPTiTiTiBPBSiThCaPTiBPBSiRnFArBPBSiRnCaFArBPRnSiRnFArRnSiRnBFArCaFArCaCaCaSiThSiThCaCaPBPTiTiRnFArCaPTiBSiAlArPBCaCaCaCaCaSiRnMgArCaSiThFArThCaSiThCaSiRnCaFYCaSiRnFYFArFArCaSiRnFYFArCaSiRnBPMgArSiThPRnFArCaSiRnFArTiRnSiRnFYFArCaSiRnBFArCaSiRnTiMgArSiThCaSiThCaFArPRnFArSiRnFArTiTiTiTiBCaCaSiRnCaCaFYFArSiThCaPTiBPTiBCaSiThSiRnMgArCaF";
@@ -38,8 +54,8 @@ int main()
 	//set<string> sources {"e"};
 
 	// Go backwards
-	set<string> sources = {"CRnCaCaCaSiRnBPTiMgArSiRnSiRnMgArSiRnCaFArTiTiBSiThFYCaFArCaCaSiThCaPBSiThSiThCaCaPTiRnPBSiThRnFArArCaCaSiThCaSiThSiRnMgArCaPTiBPRnFArSiThCaSiRnFArBCaSiRnCaPRnFArPMgYCaFArCaPTiTiTiBPBSiThCaPTiBPBSiRnFArBPBSiRnCaFArBPRnSiRnFArRnSiRnBFArCaFArCaCaCaSiThSiThCaCaPBPTiTiRnFArCaPTiBSiAlArPBCaCaCaCaCaSiRnMgArCaSiThFArThCaSiThCaSiRnCaFYCaSiRnFYFArFArCaSiRnFYFArCaSiRnBPMgArSiThPRnFArCaSiRnFArTiRnSiRnFYFArCaSiRnBFArCaSiRnTiMgArSiThCaSiThCaFArPRnFArSiRnFArTiTiTiTiBCaCaSiRnCaCaFYFArSiThCaPTiBPTiBCaSiThSiRnMgArCaF"};
-	//set<string> sources {"HOH"};
+	//set<string> sources = {"CRnCaCaCaSiRnBPTiMgArSiRnSiRnMgArSiRnCaFArTiTiBSiThFYCaFArCaCaSiThCaPBSiThSiThCaCaPTiRnPBSiThRnFArArCaCaSiThCaSiThSiRnMgArCaPTiBPRnFArSiThCaSiRnFArBCaSiRnCaPRnFArPMgYCaFArCaPTiTiTiBPBSiThCaPTiBPBSiRnFArBPBSiRnCaFArBPRnSiRnFArRnSiRnBFArCaFArCaCaCaSiThSiThCaCaPBPTiTiRnFArCaPTiBSiAlArPBCaCaCaCaCaSiRnMgArCaSiThFArThCaSiThCaSiRnCaFYCaSiRnFYFArFArCaSiRnFYFArCaSiRnBPMgArSiThPRnFArCaSiRnFArTiRnSiRnFYFArCaSiRnBFArCaSiRnTiMgArSiThCaSiThCaFArPRnFArSiRnFArTiTiTiTiBCaCaSiRnCaCaFYFArSiThCaPTiBPTiBCaSiThSiRnMgArCaF"};
+	set<string> sources {"HOHOHOHOHOHOHO"};
 	string dest = "e";
 
 	vector<pair<string,string>> replacements;
