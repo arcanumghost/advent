5933f92cedcf4cc47d702e9782f843d39d0bfe6d
arcanumghost <arcanumghost@gmail.com>
2017-12-08 15:11:04 -0600

    Day 2, both stars

--- Advent2017/Day02/main.cpp
+++ Advent2017/Day02/main.cpp
@@ -12,13 +12,28 @@ void main()
 	fstream input{ "input.txt", ios::in };
 
 	string line;
+	int checksum = 0;
 	while (getline(input, line))
 	{
-		vector<string> numbers = AGStringLib::split(line, "\t");
-		for (string number : numbers)
-			cout << number << '\n';
+		vector<string> stringNumbers = AGStringLib::split(line, "\t");
+		vector<int> intNumbers;
+		for (string& sNum : stringNumbers)
+			intNumbers.push_back(atoi(sNum.c_str()));
+
+		for (int i = 0; i<intNumbers.size()-1; ++i)
+			for (int j = i+1; j<intNumbers.size(); ++j)
+			{
+				int highNum = intNumbers[i];
+				int lowNum = intNumbers[j];
+				if (highNum < lowNum)
+					swap(lowNum, highNum);
+
+				if (highNum % lowNum == 0)
+					checksum += highNum / lowNum;
+			}
 	}
 
+	cout << checksum;
 	cin >> line;
 
 }
